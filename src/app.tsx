89c6d1a1a593934c23a7078959bae701b11a7ae8
Timur Aminev <timur.aminev@intive.com>
2024-01-30 21:20:47 +0200

    App - root react component

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,7 @@
-import { createRoot } from 'react-dom/client';
+function App() {
+  return (
+    <h2>Hello from React!</h2>
+  );
+}
 
-const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+export default App;
--- src/renderer.ts
+++ src/renderer.ts
@@ -27,6 +27,6 @@
  */
 
 import './index.css';
-import './app';
+import './index.tsx';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
